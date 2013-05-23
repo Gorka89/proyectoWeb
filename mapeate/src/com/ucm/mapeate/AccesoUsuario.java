a649d325efdff1b482da6c26304cc759b02d8286
gorka <gorka@gorka>
2013-05-23 19:34:46 +0200

    Arreglado fallo base de datos y conflictos con JDO.

--- mapeate/src/com/ucm/mapeate/AccesoUsuario.java
+++ mapeate/src/com/ucm/mapeate/AccesoUsuario.java
@@ -6,6 +6,8 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.List;
+
 import javax.jdo.PersistenceManager;
 import javax.jdo.Query;
 
@@ -37,7 +39,47 @@ public class AccesoUsuario extends HttpServlet {
         
         PersistenceManager pm = PMF.get().getPersistenceManager();
         
-        Query query = pm.newQuery(Usuarios.class);
+        Query query = pm.newQuery(Usuarios.class,
+                "name == '"+nombre+"'");
+        
+        try {
+            List<Usuarios> results = (List<Usuarios>) query.execute();
+            if (!results.isEmpty()) {
+                for (Usuarios u : results) {
+                    
+                	String name = u.getName();
+                    
+                    String ps = u.getPsw();
+                    //se compara con el md5 de la pass almacenada en la BD
+                    String md5 = md5(psw);                
+                    
+                    if (ps.equalsIgnoreCase(md5)){
+                    	
+                    	//creo la cookie con el usuario
+                    	Cookie cookie = new Cookie("username",name);
+                    	cookie.setValue(name);
+                    	cookie.setComment("probando uso cookies en servlet");
+                    	resp.addCookie(cookie);
+                    	
+                    	resp.sendRedirect("/php/visit.php");
+                    
+                    }
+                    else{
+                    	resp.sendRedirect("/mensajesFallo/invalidPass.html");                
+                    }
+                	
+                }
+            } else {
+            	resp.sendRedirect("/mensajesFallo/invalidPass.html");
+            }
+        }catch (Exception e){
+        	
+        } finally {
+            query.closeAll();
+            pm.close();
+        }
+        
+       /* Query query = pm.newQuery(Usuarios.class);
         query.setFilter("(name == myParam)");
         query.declareParameters("String myParam");
         Collection col = (Collection) query.execute(nombre);
@@ -69,11 +111,11 @@ public class AccesoUsuario extends HttpServlet {
                 }
             }
         } else {
-        	resp.sendRedirect("/mensajesFallo/usuarioInvalido.html");
+        	resp.sendRedirect("/php/visit.php");
         }
         query.closeAll();
 
-        pm.close();
+        pm.close();*/
 
     }
     
